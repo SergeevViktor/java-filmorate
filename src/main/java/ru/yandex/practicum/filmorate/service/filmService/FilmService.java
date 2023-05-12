f699265c310ec2f72885c804edbc0df91e517084
Sergeev Viktor <equalizersva@yandex.ru>
2023-05-12 19:57:42 +0300

    fix: pull req.

--- src/main/java/ru/yandex/practicum/filmorate/service/filmService/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/filmService/FilmService.java
@@ -5,11 +5,19 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmService {
+
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     List<Film> getFilms();
+
     Film getFilmById(int id);
+
     void addLike(int userId, int filmId);
+
     void deleteLike(int userId, int filmId);
+
     List<Film> getTopFilms(int count);
+
 }
